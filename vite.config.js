55fe4d944f9983200fe832ec22dee0a4fddf320c
sebastian.britos <sebastian.britos.peluffo@gmail.com>
2024-09-08 01:17:25 -0300

    cambio de version

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CoffeePage/",
+  base: "/coffeepage/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
